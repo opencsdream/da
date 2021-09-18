64593740da4fd068adb7d3176212f7b49afcdc05
XTimeglobal <90920429+XTimeglobal@users.noreply.github.com>
2021-09-18 20:48:57 +0800

    update

--- js/translate.js
+++ js/translate.js
@@ -1,5 +1,6 @@
 let zh_simple = {
 	"What is Xtime": "什么是 Xtime",
+	"What Is Xtime": "什么是 Xtime",
 	"Token Info": "代币信息",
 	"Exchange Info": "交易信息",
 	"Road Map": "路线图",
@@ -25,10 +26,23 @@ let zh_simple = {
 	"K-Line": "K线图",
 	"Road Map": "路线图",
 	"We Now Here!": "现在在这！",
-	"Language: EN":"语言：简体中文"
+	"Language: EN":"语言：简体中文",
+	"#p1":"萨尔瓦多总统Nayib Bukele，给萨尔瓦多每个人一个拥有X的机会.",
+	"#p2":"未知，因此充满激情。未知，因此拥有未来.",
+	"#p3":"这个机会希望可以通过XTime带给全世界的所有人.",
+	"#p4":"XTime Token 架构于BSC，是BSC目前唯一一个具有萨尔瓦多共和国背景的Token。XTime将致力于构建属于社区的生态系统，以BSC为基础，开发DeFi协议，将推出一个跨链收益聚合器XTime1.0，用户只需抵押其加密资产即可自动获取收益，且收益皆为BTC。XTime也将于未来推出基于BSC的NFT游戏，包括NFT集合,它们都将採用BTC作为收益获取或支付手段。",
+	"#p6":"XTime的未来目标无疑是具有包容性且强大的,在未来的路线图中XTime的目标是建立跨链金融基础设施,解锁价值互联网,同时培养强大的社区,将支持愿意将BTC以及XTime作为支付方式的公益社团。",
+	"#r1":"网站发佈<br />社区规模突破5000人<br />XTime Token 发佈<br />营销推动形成社区共识<br />跨链收益聚合器XTime1.0开发",
+	"#r2":"影响力营销持续推动<br /><br />完成第三方审计<br />社区规模突破10000人<br />持币地址突破1000<br />跨链收益聚合器XTime1.0推出",
+	"#r3":"世界级主流媒体影响力营销推动<br />社区规模突破50000人<br />持币地址突破10000<br />跨链收益聚合器XTime2.0升级<br />NFT集合进入审计<br />通过联合国儿童基金会进行第一次捐赠",
+	"#r4":"世界级主流媒体影响力营销持续推动<br />社区规模突破100000人<br />跨链收益聚合器XTime2.0推出<br />NFT集合推出",
+	"#r5":"增加营销推动<br />社区规模突破150000人<br />持币地址突破100000<br />跨链收益聚合器XTime3.0推出<br />NFT游戏代码审计并推出<br />通过联合国儿童基金会进行第二次捐赠",
+	"#r6":"持续营销推动<br />社区规模突破200000人<br />持币地址突破200000<br />通过联合国儿童基金会进行第三次捐赠<br />重新设计网站并推出新路线图",
+	
 }
 let zh_wawa = {
 	"What is Xtime": "什麽是 Xtime",
+	"What Is Xtime": "什麽是 Xtime",
 	"Token Info": "代幣信息",
 	"Exchange Info": "交易信息",
 	"Road Map": "路線圖",
@@ -59,6 +73,7 @@ let zh_wawa = {
 
 let jap = {
 	"What is Xtime": "Xtimeとは何か、",
+	"What Is Xtime": "Xtimeとは何か、",
 	"Token Info": "代行情報",
 	"Exchange Info": "取引情報",
 	"Road Map": "道路地図",
@@ -108,14 +123,13 @@ window.translate = function(i) {
 				node.stext = tran.innerText
 				tran.innerHTML = pack[tran.innerText]
 				node.ele = tran;
-
 				elenodes.push(node)
 			} else {
 				if (pack["#" + tran.id]) {
 					let node = new Object();
 					node.source = tran.id;
 					node.stext = tran.innerText;
-					tran.innerHTML = pack[tran.innerText]
+					tran.innerHTML = pack["#" + tran.id]
 					node.ele = tran;
 					elenodes.push(node)
 				}
