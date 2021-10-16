6a780e55a056613212c6e9de4250dad49b6436df
opencsdream <opencsdream@gmail.com>
2021-10-16 22:41:08 +0800

    new pages

---
+++ countdown/assets/js/scripts.js
@@ -0,0 +1,37 @@
+
+jQuery(document).ready(function() {
+
+    /*
+        Background slideshow
+    */
+    // $('.coming-soon').backstretch([
+    //   "../img/countdown/top.jpg"
+    // ]);
+
+    /*
+        Countdown initializer
+    */
+    var now = new Date();
+    var finaldate = new Date("NOV 10 2021 14:00 UTC ")
+    var countTo = finaldate.getTime() - now.getTime() + now.valueOf();
+    $('.timer').countdown(countTo, function(event) {
+        var $this = $(this);
+        switch(event.type) {
+            case "seconds":
+            case "minutes":
+            case "hours":
+            case "days":
+            case "weeks":
+            case "daysLeft":
+                $this.find('span.'+event.type).html(event.value);
+                break;
+            case "finished":
+                $this.hide();
+                break;
+        }
+    });
+
+
+
+});
+
